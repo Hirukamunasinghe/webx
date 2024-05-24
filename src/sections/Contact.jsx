8ebe2106b98b7680aba79a0be9b81604ed9a7d48
Hirukamunasinghe <munasinghehiruka@gmail.com>
2024-05-24 15:11:28 +0530

    contact form

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -14,10 +14,10 @@ const Contact = () =>{
                     <h1 className="text-4xl text-headingColor">CONTACT US</h1>
                     <h1 className="text-3xl text-subColor font-bold">REACH OUT FOR A NEW PROJECT!</h1>
                     <form className="form text-white flex flex-col gap-[25px] " action="https://formsubmit.co/munasinghehiruka@gmail.com" method="POST">
-                        <input className="bg-customPurple w-[450px] h-[60px] rounded-sm pl-[10px] placeholder-white" type="text" id="name" placeholder="Name" required data-validation-required-message="Please enter your name."></input>
-                        <input className="bg-customPurple w-[450px] h-[60px] rounded-sm pl-[10px] placeholder-white" type="email" id="email" placeholder="Email" required data-validation-required-message="Please enter your email."></input>
-                        <input className="bg-customPurple w-[450px] h-[60px] rounded-sm pl-[10px] placeholder-white" type="phone" id="phone" placeholder="Phone" data-validation-required-message="Please enter your phone number."></input>
-                        <input className="bg-customPurple w-[450px] h-[180px] pl-[10px] rounded-sm pb-[120px]  placeholder-white" type="text" id="text" placeholder="Message" data-validation-required-message="Please enter your message."></input>
+                        <input className="bg-customPurple w-[450px] h-[60px] rounded-sm pl-[10px] placeholder-white" type="text" name="name" id="name" placeholder="Name" required data-validation-required-message="Please enter your name."></input>
+                        <input className="bg-customPurple w-[450px] h-[60px] rounded-sm pl-[10px] placeholder-white" type="email" name="email" id="email" placeholder="Email" required data-validation-required-message="Please enter your email."></input>
+                        <input className="bg-customPurple w-[450px] h-[60px] rounded-sm pl-[10px] placeholder-white" type="phone" name="name" id="phone" placeholder="Phone" data-validation-required-message="Please enter your phone number."></input>
+                        <input className="bg-customPurple w-[450px] h-[180px] pl-[10px] rounded-sm pb-[120px]  placeholder-white" type="text" id="text" name="message" placeholder="Message" data-validation-required-message="Please enter your message."></input>
                         <button type="submit" className="h-12 rounded-md font-bold hover:text-white hover:bg-purple-500 transition duration-2000 ease-in-out bg-customPurple w-48">Send</button>
                     </form>
                 </div>
