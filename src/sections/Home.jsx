c8f52f3f79520eea691e25b0a96290e3ca54bafb
Hirukamunasinghe <munasinghehiruka@gmail.com>
2024-05-26 03:43:45 +0530

    commit

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -20,13 +20,13 @@ const Home = () => {
   return (
     <div>
       <Navbar/>
-      <div className="bg-hero-pattern w-full h-[295px] md:h-[500px] lg:h-[1270px]  lg:mt-[-369px] bg-cover bg-no-repeat"></div>
-      <div className="flex justify-center mt-[-270px] md:mt-[-400px] lg:mt-[-830px] sm:mt-[-300px]">
-        <div className={`text-sm md:text-xl lg:text-6xl text-left lg:text-center lg:pt-[0px] text-customWhite font-bold typewriter-text transition-opacity duration-1000 ${showText ? 'opacity-100' : 'opacity-0'}`}>
+      <div className="bg-hero-pattern w-full h-[290px] lg:h-[1200px] md:h-[580px] lg:mt-[-369px] bg-cover bg-no-repeat"></div>
+      <div className="flex justify-center mt-[-270px] md:mt-[-480px] lg:mt-[-780px] sm:mt-[-300px]">
+        <div className={`text-sm md:text-2xl lg:text-6xl text-left lg:text-center lg:pt-[0px] text-customWhite font-bold typewriter-text transition-opacity duration-1000 ${showText ? 'opacity-100' : 'opacity-0'}`}>
           TRANSFORMING CONCEPTS INTO REALITY
         </div>
       </div>
-      <div className="mt-[240px] lg:mt-[770px] sm:mt-[360px]">
+      <div className="md:mt-[420px] lg:mt-[720px] mt-[230px]">
         <Services/>
         <Technologies/>
         <ContactDisplay/>
