b040adc2e0d9ee22e09aa7cc2ba0e3c6d87c1eeb
Hirukamunasinghe <munasinghehiruka@gmail.com>
2024-05-26 04:34:20 +0530

    commit

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -16,14 +16,14 @@ const Contact = () => {
             <Navbar />
             <div className="contactDiv">
                 <div className="card flex justify-center">
-                    <div className="h-[650px] w-[350px] lg:w-[1000px] bg-hero-contact border-2 mt-[25px] flex flex-col gap-[10px] pl-[10px] pt-[20px]">
-                        <h1 className="text-4xl text-headingColor">CONTACT US</h1>
-                        <h1 className="text-3xl text-subColor font-bold">REACH OUT FOR A NEW PROJECT!</h1>
-                        <form className="form text-white flex flex-col gap-[25px]" action="https://formsubmit.co/munasinghehiruka@gmail.com" method="POST">
-                            <input className="bg-customPurple w-[310px] lg:w-[450px] h-[60px] rounded-sm pl-[10px] placeholder-white" type="text" name="name" id="name" placeholder="Name" required data-validation-required-message="Please enter your name." />
-                            <input className="bg-customPurple w-[310px] lg:w-[450px] h-[60px] rounded-sm pl-[10px] placeholder-white" type="email" name="email" id="email" placeholder="Email" required data-validation-required-message="Please enter your email." />
-                            <input className="bg-customPurple w-[310px] lg:w-[450px] h-[60px] rounded-sm pl-[10px] placeholder-white" type="phone" name="phone" id="phone" placeholder="Phone" data-validation-required-message="Please enter your phone number." />
-                            <input className="bg-customPurple w-[310px] lg:w-[450px] h-[150px] lg:h-[180px] pl-[10px] rounded-sm pb-[120px] placeholder-white" type="text" id="text" name="message" placeholder="Message" data-validation-required-message="Please enter your message." />
+                    <div className="h-[650px] w-[350px] lg:w-[1000px] bg-hero-contact bg-cover bg-no-repeat border-2 mt-[25px] lg:flex flex-col items-center lg:items-start  gap-[10px] pl-[10px] pt-[20px]">
+                        <h1 className="text-4xl text-headingColor text-center lg:text-left">CONTACT US</h1>
+                        <h1 className="text-3xl text-subColor font-bold text-center lg:text-left">REACH OUT FOR A NEW PROJECT!</h1>
+                        <form className="form text-white flex flex-col items-center lg:items-start mt-[20px] gap-[25px]" action="https://formsubmit.co/munasinghehiruka@gmail.com" method="POST">
+                            <input className="bg-customPurple w-[300px] lg:w-[450px] h-[60px] rounded-sm pl-[10px] placeholder-white" type="text" name="name" id="name" placeholder="Name" required data-validation-required-message="Please enter your name." />
+                            <input className="bg-customPurple w-[300px] lg:w-[450px] h-[60px] rounded-sm pl-[10px] placeholder-white" type="email" name="email" id="email" placeholder="Email" required data-validation-required-message="Please enter your email." />
+                            <input className="bg-customPurple w-[300px] lg:w-[450px] h-[60px] rounded-sm pl-[10px] placeholder-white" type="phone" name="phone" id="phone" placeholder="Phone" data-validation-required-message="Please enter your phone number." />
+                            <input className="bg-customPurple w-[300px] lg:w-[450px] h-[150px] lg:h-[180px] pl-[10px] rounded-sm pb-[120px] placeholder-white" type="text" id="text" name="message" placeholder="Message" data-validation-required-message="Please enter your message." />
                             <button type="submit" className="h-12 rounded-md font-bold hover:text-white hover:bg-purple-500 transition duration-2000 ease-in-out bg-customPurple w-48">Send</button>
                         </form>
                     </div>
