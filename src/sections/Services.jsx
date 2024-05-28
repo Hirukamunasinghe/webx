f0d5a1f96ef0b0ba4e62591f55060f35ae143b81
Hirukamunasinghe <munasinghehiruka@gmail.com>
2024-05-28 19:59:47 +0530

    commit

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -9,21 +9,27 @@ import { Link } from "react-router-dom";
 const AnimatedDesign = withIntersectionObserver(() => (
   <div className="design flex flex-col lg:flex-row justify-center gap-5 lg:gap-[200px] border-2 rounded-md border-customPurple p-5 lg:p-[50px] w-[350px] md:w-[700px] lg:w-[1800px]">
     <div className="design-image">
-      <img src={Design} className="h-[200px] md:h-[400px] lg:h-[500px] rounded-md" />
+      <img
+        src={Design}
+        className="h-[200px] md:h-[400px] lg:h-[500px] rounded-md"
+      />
     </div>
     <div className="textContent flex flex-col gap-5 lg:gap-[80px]">
-      <h1 className="text-3xl lg:text-5xl font-bold text-subColor">WEB DESIGN</h1>
-      <p className="text-customPurple leading-6 lg:leading-10">
-        At WebX, we deliver web design services that blend aesthetic appeal
+      <h1 className="text-3xl lg:text-5xl font-bold text-subColor">
+        WEB DESIGN
+      </h1>
+      <p className="text-customPurple text-sm lg:text-md md:text-lg leading-5 lg:leading-10">
+        At WebX, we deliver web design services that fuse captivating aesthetics
         <br />
-        with seamless functionality. Our mission is to create websites that not
+        with flawless functionality. Our mission is to craft websites that not
         only
         <br />
-        capture attention but also drive engagement and conversions.
+        captivate attention but also ignite engagement and drive conversions.
         <br />
-        With a focus on your unique business needs, we ensure your
-        <br /> online presence stands out and makes a lasting impact
-        <br /> in the digital landscape.
+        With an unwavering focus on your distinctive business needs, we
+        ensure unparallel excellence
+
+
       </p>
       <Link to="/projects">
         <Button backgroundColor="bg-customPurple" color="text-white">
@@ -37,11 +43,16 @@ const AnimatedDesign = withIntersectionObserver(() => (
 const AnimatedDevelopment = withIntersectionObserver(() => (
   <div className="develop flex flex-col lg:flex-row-reverse justify-center gap-5 lg:gap-[200px] border-2 rounded-md border-customPurple p-5 lg:p-[50px] w-[350px] md:w-[700px] lg:w-[1800px]">
     <div className="design-image">
-      <img src={Development} className="h-[200px] md:h-[400px] lg:h-[500px] ml-0 lg:ml-[50px] rounded-md" />
+      <img
+        src={Development}
+        className="h-[200px] md:h-[400px] lg:h-[500px] ml-0 lg:ml-[50px] rounded-md"
+      />
     </div>
     <div className="textContent flex flex-col gap-5 lg:gap-[80px]">
-      <h1 className="text-3xl lg:text-5xl text-subColor font-bold">WEB DEVELOPMENT</h1>
-      <p className="text-customPurple leading-6 lg:leading-10">
+      <h1 className="text-3xl lg:text-5xl text-subColor font-bold">
+        WEB DEVELOPMENT
+      </h1>
+      <p className="text-customPurple text-sm lg:text-lg md:text-lg leading-6 lg:leading-10">
         At WebX, we offer web development services that combine innovative
         <br /> technology with user-focused design. Our goal is to build
         websites
@@ -64,18 +75,26 @@ const AnimatedDevelopment = withIntersectionObserver(() => (
 const AnimatedPortfolio = withIntersectionObserver(() => (
   <div className="portfolio flex flex-col lg:flex-row justify-center gap-5 lg:gap-[200px] border-2 rounded-md border-customPurple p-5 lg:p-[50px] w-[350px] md:w-[700px] lg:w-[1800px]">
     <div className="design-image">
-      <img src={Portfolio} className="h-[200px] md:h-[400px] lg:h-[500px] rounded-md" />
+      <img
+        src={Portfolio}
+        className="h-[200px] md:h-[400px] lg:h-[500px] rounded-md"
+      />
     </div>
     <div className="textContent flex flex-col gap-5 lg:gap-[80px]">
-      <h1 className="text-3xl lg:text-5xl font-bold text-subColor">PORTFOLIO DEVELOPMENT</h1>
-      <p className="text-customPurple leading-6 lg:leading-10">
+      <h1 className="text-3xl lg:text-5xl font-bold text-subColor">
+        PORTFOLIO DEVELOPMENT
+      </h1>
+      <p className="text-customPurple text-sm lg:text-lg md:text-lg leading-6 lg:leading-10">
         At WebX, we specialize in crafting visually striking
         <br />
-        and user-friendly portfolio websites that not only showcase your work but also
+        and user-friendly portfolio websites that not only showcase your work
+        but also
         <br />
-        function seamlessly. Our portfolio site development services are tailored to meet
+        function seamlessly. Our portfolio site development services are
+        tailored to meet
         <br />
-        the unique needs of your brand, ensuring that your online portfolio stands out
+        the unique needs of your brand, ensuring that your online portfolio
+        stands out
         <br />
         in a crowded digital landscape.
       </p>
@@ -92,7 +111,9 @@ const Services = () => {
   return (
     <div id="services">
       <div className="services-div  pt-[70px] bg-sectionBg">
-        <h1 className="text-4xl lg:text-6xl text-center text-headingColor font-bold pb-[20px]">SERVICES</h1>
+        <h1 className="text-4xl lg:text-6xl text-center text-headingColor font-bold pb-[20px]">
+          SERVICES
+        </h1>
         <div className="services grid place-items-center gap-10 lg:gap-[120px] mt-[50px]">
           <AnimatedDesign />
           <AnimatedDevelopment />
